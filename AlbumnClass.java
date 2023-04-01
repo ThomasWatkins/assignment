5babdb098e67a3aea4867c17c0ceaebab8d3c249
ThomasWatkins <129256055+ThomasWatkins@users.noreply.github.com>
2023-04-01 12:53:21 +1100

    classes

--- AlbumnClass.java
+++ AlbumnClass.java
@@ -3,22 +3,22 @@
  *Student No: 3442737
  *Date Started: 27/3/23
  *Description: 
- * A Class for sorting methods and storing information about the album section of the SongCollection Program
+ * A Class for storing methods and information about the album object for the SongCollection Program
  */ 
 
 
 public class AlbumClass {
 
-    //Storing information on the album//
-    private String albumName
-    private new song = song1 
-    private new song = song2 
-    private new song = song3 
-    private new song = song4 
-    private int albumTime
+    //initialising instance variables for the album object//
+    private String albumName;
+    private new song = song1;
+    private new song = song2;
+    private new song = song3;
+    private new song = song4;
+    private int albumTime;
     private int maxAlbumTime = 1200
 
-//Initialize a new Album name to the empty string and empty song data
+//constructor Album to the empty string and empty interior data
     public Album()
     {
     albumName = "No information added yet";
@@ -32,9 +32,9 @@ public class AlbumClass {
 
 //other methods
     //set an album name
-    public void setAlbumName (String nm)
+    public void setAlbumName (String am)
     {
-        albumName = nm
+        albumName = am
     }
     
     //get an album name
